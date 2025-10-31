ae111469af33e5142a1ec946b8e76fc845c5a528
leolsm12 <leonardo.schaffer.mota@outlook.com>
2025-10-31 18:26:13 -0300

    Adiciona estrutura básicado backend API

---
+++ godlevel-api/src/main/java/com/godlevel_api/GodlevelApiApplication.java
@@ -0,0 +1,13 @@
+package com.godlevel_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GodlevelApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GodlevelApiApplication.class, args);
+	}
+
+}
---
+++ godlevel-api/src/test/java/com/godlevel_api/GodlevelApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.godlevel_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GodlevelApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
